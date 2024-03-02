9fdb71e0f44fd439dbf58c20f47d3da7d8a38dd8
Treesound <118625143+Treesound@users.noreply.github.com>
2024-03-02 14:26:51 +0200

    task 1

--- code/index.php
+++ code/index.php
@@ -1,2 +1,11 @@
 <?php
-echo phpinfo();
+//task 1 - access by reference
+echo "<b>task 1</b><br>";
+$very_bad_unclear_name = "15 chicken wings";
+
+$order = &$very_bad_unclear_name;
+$order .= " and 2 bottles of soda";
+
+echo "\nYour order is: $very_bad_unclear_name.";
+
+?>
